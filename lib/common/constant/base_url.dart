5ae97c524199aebc253fbec4090c9a7bb87391c2
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2022-08-05 22:48:28 +0700

    🔧 Added Common constant value

---
+++ lib/common/constant/base_url.dart
@@ -0,0 +1,4 @@
+class BaseUrl {
+  static const String baseUrlIcons = "assets/icons";
+  static const String baseUrlBackground = "assets/background";
+}
---
+++ lib/common/constant/url_asset.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_bil_hikmah/common/constant/base_url.dart';
+
+class UrlAsset {
+  static const String _baseUrlIcons = BaseUrl.baseUrlIcons;
+  static const String _baseUrlBackground = BaseUrl.baseUrlBackground;
+
+  static const String logo = "$_baseUrlIcons/logo.png";
+
+  static const List<String> onBoardingImage = [
+    "$_baseUrlBackground/onboard1.png",
+    "$_baseUrlBackground/onboard2.png",
+    "$_baseUrlBackground/onboard3.png",
+  ];
+
+  static const String homeBanner = "$_baseUrlBackground/home_banner.png";
+  static const String alQuran = "$_baseUrlIcons/alquran.png";
+  static const String book = "$_baseUrlIcons/book.png";
+  static const String detailQuranBanner =
+      "$_baseUrlBackground/detail_quran_banner.png";
+  static const String signLanguageBanner =
+      "$_baseUrlBackground/sign_language_banner.png";
+  static const String frameAlquranNumber =
+      "$_baseUrlBackground/frame_alquran_number.png";
+  static const String heart = "$_baseUrlIcons/heart.png";
+  static const String info = "$_baseUrlIcons/info.png";
+  static const String setting = "$_baseUrlIcons/setting.png";
+  static const String more = "$_baseUrlIcons/more.png";
+  static const String question = "$_baseUrlIcons/question.png";
+  static const String privacy = "$_baseUrlIcons/privacy.png";
+  static const String star = "$_baseUrlIcons/star.png";
+  static const String logout = "$_baseUrlIcons/logout.png";
+}
