7b63b84a0393e0778f7c925be2854070c54c2952
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2022-08-08 20:45:28 +0700

    💄 Added bottom navigation bar - Dashbaord

---
+++ lib/feature/dashboard/screen/section/navigation_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bil_hikmah/style/colors.dart';
+import 'package:flutter_bil_hikmah/style/text.dart';
+import 'package:flutter_svg/svg.dart';
+
+class NavigationItem extends StatelessWidget {
+  const NavigationItem(this.callback, this.active, this.svgSource, this.label,
+      {Key? key})
+      : super(key: key);
+
+  final VoidCallback callback;
+  final bool active;
+  final String svgSource;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        IconButton(
+            padding: EdgeInsets.zero,
+            onPressed: callback,
+            iconSize: 24.0,
+            icon: SvgPicture.asset(
+              svgSource,
+              color: active ? AppColors.primaryDark : AppColors.lightGreyMedium,
+            )),
+        Text(
+          label,
+          style: AppTextStyle.textExtraSmall.copyWith(
+            color: active ? AppColors.primaryDark : AppColors.lightGreyMedium,
+            height: 0,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.3,
+          ),
+        ),
+        const SizedBox(height: 10.0),
+      ],
+    );
+  }
+}
