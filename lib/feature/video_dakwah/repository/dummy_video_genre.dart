89c7d6b5163e72f0c4783747d959cab86fa7cc14
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2022-08-30 19:59:16 +0700

    🔧 Added dummy repositoru

---
+++ lib/feature/video_dakwah/repository/dummy_video_genre.dart
@@ -0,0 +1,14 @@
+class DummyVideoGenre {
+  String title;
+
+  DummyVideoGenre(this.title);
+}
+
+final List<DummyVideoGenre> listGenreVideo = [
+  DummyVideoGenre("Semua"),
+  DummyVideoGenre("Kajian"),
+  DummyVideoGenre("Kultum"),
+  DummyVideoGenre("Pendidikan"),
+  DummyVideoGenre("Pengajian"),
+  DummyVideoGenre("Bahasa Isyarat"),
+];
---
+++ lib/feature/video_dakwah/repository/dummy_video_item.dart
@@ -0,0 +1,46 @@
+class DummyVideoItem {
+  String urlVideo;
+  String title;
+  String genreVideo;
+  int countView;
+  DateTime date;
+
+  DummyVideoItem(
+    this.urlVideo,
+    this.title,
+    this.genreVideo,
+    this.countView,
+    this.date,
+  );
+}
+
+final List<DummyVideoItem> dummyListVideoItem = [
+  DummyVideoItem(
+    "",
+    "Melangkah Menuju Hidup Islami yang Berseri Menyambut Indahnya Ramadhan 1432 H",
+    "Kultum",
+    123321,
+    DateTime.now(),
+  ),
+  DummyVideoItem(
+    "",
+    "Melangkah Menuju Hidup Islami yang Berseri Menyambut Indahnya Ramadhan 1432 H",
+    "Kultum",
+    123321,
+    DateTime.now(),
+  ),
+  DummyVideoItem(
+    "",
+    "Melangkah Menuju Hidup Islami yang Berseri Menyambut Indahnya Ramadhan 1432 H",
+    "Kultum",
+    123321,
+    DateTime.now(),
+  ),
+  DummyVideoItem(
+    "",
+    "Melangkah Menuju Hidup Islami yang Berseri Menyambut Indahnya Ramadhan 1432 H",
+    "Kultum",
+    123321,
+    DateTime.now(),
+  ),
+];
