73d7d3c90f3bebd1e2e1661d51fd09f0d39b2515
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2022-09-14 19:34:54 +0700

    🎨 improve common widget

--- lib/feature/akhlak_mulia/screen/section/akhlak_mulia_view.dart
+++ lib/feature/akhlak_mulia/screen/section/akhlak_mulia_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bil_hikmah/style/colors.dart';
-import 'package:flutter_bil_hikmah/style/text.dart';
+
+import 'package:flutter_bil_hikmah/widget/field/default_card_item.dart';
 
 class AkhlakMuliaView extends StatefulWidget {
   const AkhlakMuliaView({Key? key}) : super(key: key);
@@ -12,39 +12,6 @@ class AkhlakMuliaView extends StatefulWidget {
 class _AkhlakMuliaViewState extends State<AkhlakMuliaView> {
   @override
   Widget build(BuildContext context) {
-    Widget _akhlakMuliaContainerItem() => InkWell(
-          onTap: () {},
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 24,
-              vertical: 29,
-            ),
-            decoration: const BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                  color: AppColors.lightGreyLight,
-                  width: 1,
-                ),
-              ),
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    "Akhlak Mulia",
-                    style: AppTextStyle.textLarge
-                        .copyWith(color: AppColors.darkGreyDark),
-                  ),
-                ),
-                const Icon(
-                  Icons.arrow_forward_ios_rounded,
-                  color: AppColors.primaryDark,
-                ),
-              ],
-            ),
-          ),
-        );
-
     return ListView.builder(
       padding: const EdgeInsets.symmetric(horizontal: 24),
       itemCount: 5,
@@ -52,9 +19,9 @@ class _AkhlakMuliaViewState extends State<AkhlakMuliaView> {
         return index == 0
             ? Padding(
                 padding: const EdgeInsets.only(top: 24.0),
-                child: _akhlakMuliaContainerItem(),
+                child: defaultItemCard("Akhlak Mulia", () {}),
               )
-            : _akhlakMuliaContainerItem();
+            : defaultItemCard("Akhlak Mulia", () {});
       },
     );
   }
