abd8d764dbe77368c6e6ff5d097ea99b14507cf8
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2022-09-17 15:53:40 +0700

    🔧 Added common dummy data

---
+++ lib/common/constant/dummy.dart
@@ -0,0 +1,8 @@
+class Dummy {
+  static const String dummyContentText = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet ex efficitur, pretium turpis sit amet, maximus magna. Morbi ante tellus, sodales sit amet pretium ut, auctor et neque. Curabitur aliquet massa sed nisl tempor mattis. Donec ex metus, facilisis id diam et, fermentum egestas neque. Morbi porttitor tortor sed arcu auctor, a gravida nisl auctor. Integer condimentum, lorem eget dapibus pulvinar, neque leo facilisis libero, ut tristique eros ante ac tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis dui non convallis mattis. Integer mollis pellentesque ultricies. Donec eleifend fringilla lectus eget auctor. Sed sit amet ante iaculis, congue tortor non, scelerisque lectus.
+
+Pellentesque pulvinar sed ex vitae convallis. Quisque ac risus neque. Fusce in massa et diam volutpat molestie non vitae augue. Maecenas enim est, laoreet eu pharetra quis, mollis sit amet arcu. Praesent molestie tellus eu velit imperdiet pellentesque. Donec gravida consectetur nisl sit amet tristique. Curabitur nec posuere velit. Aliquam fringilla, ligula at elementum efficitur, quam dolor consequat purus, ut feugiat augue tortor at lacus. Etiam auctor finibus turpis, vitae dapibus lorem congue non.
+
+Donec vestibulum ligula vitae lacus elementum, id tempus mauris pretium. Curabitur suscipit malesuada posuere. Curabitur tristique risus vel dapibus eleifend. Duis viverra hendrerit arcu, non congue nibh dapibus ac. Morbi luctus. """;
+}
