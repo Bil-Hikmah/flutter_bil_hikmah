934c6a6af39f598627d9c2e40b1ec2d9f98f7618
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2022-10-04 21:20:52 +0700

    🔒️ Allow bad certificate

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bil_hikmah/common/provider/app_bloc_provider.dart';
 import 'package:flutter_bil_hikmah/config/routes/route_handler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:io' as dart_io;
+
+class MyHttpOverrides extends dart_io.HttpOverrides {
+  @override
+  dart_io.HttpClient createHttpClient(dart_io.SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (dart_io.X509Certificate cert, String host, int port) => true;
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  dart_io.HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
