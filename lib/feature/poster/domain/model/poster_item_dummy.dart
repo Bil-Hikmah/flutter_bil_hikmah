9b4abfff16435f1ef9adebc0d11c88eaa3b945d0
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2022-10-05 20:39:24 +0700

    🔧 Added poster dummy data

---
+++ lib/feature/poster/domain/model/poster_item_dummy.dart
@@ -0,0 +1,18 @@
+class PosterItem {
+  String urlImage;
+
+  PosterItem(this.urlImage);
+}
+
+final List<PosterItem> posterItemDummy = [
+  PosterItem(
+      "https://i.pinimg.com/originals/b6/e0/45/b6e045adff2d6c6b6b2fd698fb38c27f.jpg"),
+  PosterItem(
+      "https://i.pinimg.com/originals/35/51/3a/35513a9e854827df60de80c6864ca454.jpg"),
+  PosterItem(
+      "https://1.bp.blogspot.com/-QYP2fC_lorY/YFyS07WaVHI/AAAAAAAAAFk/F3OtOLWzo0E15aH-lrusUT6NMHE8XL2zQCLcBGAsYHQ/s2048/3E96BCD4-A8B6-4253-9110-5DE6E82BACBB.jpeg"),
+  PosterItem(
+      "http://2.bp.blogspot.com/-aT3H-lhgSpM/Vo0b2iZvUxI/AAAAAAAAAMQ/H69bTwWH2Zc/s1600/POSTER%2BBARU.jpg"),
+  PosterItem(
+      "https://alfityah.com/wp/wp-content/uploads/2018/10/39281840_323535358406937_3684594654453432320_n.jpg"),
+];
