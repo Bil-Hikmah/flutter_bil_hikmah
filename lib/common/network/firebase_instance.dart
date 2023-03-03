46e3b24001e726172386ce1115b22e561568dcb8
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2023-03-03 22:41:48 +0700

    🔧 instance firebase storage

--- lib/common/network/firebase_instance.dart
+++ lib/common/network/firebase_instance.dart
@@ -1,11 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class FirebaseBilHikmah {
-  const FirebaseBilHikmah(this.firestore);
+  const FirebaseBilHikmah(
+    this.firestore,
+    this.fireStorage,
+  );
 
   final FirebaseFirestore firestore;
+  final FirebaseStorage fireStorage;
 
   factory FirebaseBilHikmah.injection() {
-    return FirebaseBilHikmah(FirebaseFirestore.instance);
+    return FirebaseBilHikmah(
+      FirebaseFirestore.instance,
+      FirebaseStorage.instance,
+    );
   }
 }
