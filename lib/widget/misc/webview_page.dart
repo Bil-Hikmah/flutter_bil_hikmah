d8eeeef359f59f6fc036155f1fb317205632e1c4
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2023-03-06 21:05:37 +0700

    🎨 Added common webview

---
+++ lib/widget/misc/webview_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bil_hikmah/style/colors.dart';
+import 'package:flutter_bil_hikmah/style/text.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+enum WebViewKLocalHTML {
+  termAndConditions,
+  privacyPolicy,
+  disable,
+}
+
+class WebviewPage extends StatefulWidget {
+  const WebviewPage({
+    Key? key,
+    required this.title,
+    this.link,
+    this.javascriptMode = JavascriptMode.disabled,
+    this.webViewKLocalHTML = WebViewKLocalHTML.disable,
+  }) : super(key: key);
+
+  final String title;
+  final String? link;
+  final JavascriptMode javascriptMode;
+  final WebViewKLocalHTML webViewKLocalHTML;
+
+  @override
+  State<WebviewPage> createState() => _WebviewPageState();
+}
+
+class _WebviewPageState extends State<WebviewPage> {
+  double current = 0;
+  late WebViewController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: AppColors.darkGreyDark,
+            ),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          elevation: 0,
+          backgroundColor: Colors.white,
+          title: Text(widget.title, style: AppTextStyle.textMedium),
+          actions: [
+            IconButton(
+              onPressed: () {
+                if (widget.webViewKLocalHTML == WebViewKLocalHTML.disable) {
+                  _controller.reload();
+                }
+              },
+              icon: const Icon(
+                Icons.refresh,
+                color: AppColors.darkGreyDark,
+              ),
+            )
+          ],
+        ),
+        body: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                LinearProgressIndicator(
+                  color: current == 1.0 ? Colors.white : AppColors.primaryDark,
+                  backgroundColor: AppColors.lightGreyMedium,
+                  value: current,
+                  semanticsLabel: "Loading",
+                  minHeight: 2.0,
+                ),
+                Expanded(
+                  child: WebView(
+                    initialUrl: widget.link ?? 'about:blank',
+                    javascriptMode: widget.javascriptMode,
+                    onWebViewCreated: (controller) async {
+                      _controller = controller;
+                    },
+                    onProgress: (value) {
+                      setState(() {
+                        current = value / 100;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Visibility(
+              visible: current == 1.0 ? false : true,
+              child: Center(
+                child: Text(
+                  "Loading...",
+                  style: AppTextStyle.textMedium
+                      .copyWith(color: AppColors.lightGreyDark),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
