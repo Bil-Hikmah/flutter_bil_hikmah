b8992dabae29029e56f1d5dcf0685517322bf4a4
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2023-03-08 20:14:44 +0700

    🔧 remove unused navigation to amalan sunnah detail page

--- lib/feature/amalan_sunnah/screen/section/amalan_sunnah_view.dart
+++ lib/feature/amalan_sunnah/screen/section/amalan_sunnah_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bil_hikmah/feature/amalan_sunnah/logic/amalan_sunnah_cubit.dart';
-import 'package:flutter_bil_hikmah/feature/default_detail/default_detail_page.dart';
 import 'package:flutter_bil_hikmah/widget/field/default_card_item.dart';
+import 'package:flutter_bil_hikmah/widget/misc/webview_page.dart';
 
 class AmalanSunnahView extends StatefulWidget {
   const AmalanSunnahView(this.state, {Key? key}) : super(key: key);
@@ -22,9 +22,11 @@ class _AmalanSunnahViewState extends State<AmalanSunnahView> {
         return defaultItemCard(
           widget.state.amalanSunnahItem[index].title,
           () {
-            Navigator.of(context).push(DefaultDetailPage.route(
-              widget.state.amalanSunnahItem[index].title,
-              widget.state.amalanSunnahItem[index].webURL,
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => WebviewPage(
+                title: widget.state.amalanSunnahItem[index].title,
+                link: widget.state.amalanSunnahItem[index].webURL,
+              ),
             ));
           },
         );
