5655be26f56e38fbd28ecf8ad78024f53ba14106
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2023-04-27 19:58:44 +0700

    🔧 update firebase instance

--- lib/common/network/firebase_instance.dart
+++ lib/common/network/firebase_instance.dart
@@ -1,19 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 class FirebaseBilHikmah {
   const FirebaseBilHikmah(
     this.firestore,
     this.fireStorage,
+    this.firebaseAuthentication,
   );
 
   final FirebaseFirestore firestore;
   final FirebaseStorage fireStorage;
+  final FirebaseAuth firebaseAuthentication;
 
   factory FirebaseBilHikmah.injection() {
     return FirebaseBilHikmah(
       FirebaseFirestore.instance,
       FirebaseStorage.instance,
+      FirebaseAuth.instance,
     );
   }
 }
